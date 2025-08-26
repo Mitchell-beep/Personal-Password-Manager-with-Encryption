e224897d43549a5b6405ec2f17bd29535bab73aa
vboxuser <vboxuser@WINDOWS11>
2025-08-26 12:14:33 -0500

    Add project files.

---
+++ Personal Password Manager with Encryption/Form1.cs
@@ -0,0 +1,10 @@
+namespace Personal_Password_Manager_with_Encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
